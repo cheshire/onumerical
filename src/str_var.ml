5e0f283c863bc5e4c6db27271048c54668d3b368
George Karpenkov <george@metaworld.ru>
2014-04-01 15:38:05 +0200

    Docs

--- src/str_var.ml
+++
@@ -1,8 +0,0 @@
-open Core.Std
-
-module S:(module type of Var_intf with type t = string) = struct
-    type t = string with sexp
-    let to_string s = s
-end
-
-include S
--- src/var_intf.mli
+++
@@ -1,7 +0,0 @@
-(** Variable type: anything serializable and comparable *)
-open Core.Std
-
-(** Variable datatype: serializable, polymorphic comparison is used. *)
-type t with sexp
-
-val to_string : t -> string
